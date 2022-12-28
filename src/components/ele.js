0d626fa1f32154356f81e841c07308ebcdfda3f2
Shreya Jash <shreyajash98@gmail.com>
2022-12-28 16:39:14 +0530

    firebaseupdate

---
+++ src/components/ele.js
@@ -0,0 +1,19 @@
+const data =[
+    {
+        id:1,
+        name:"ronaldo"
+    },
+    {
+        id:2,
+        name:"messi"
+    },
+    {
+        id:3,
+        name:"pele"
+    },
+     {
+        id:4,
+        name:"mbappe"
+    }
+]
+export default data;
